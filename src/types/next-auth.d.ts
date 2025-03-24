e68ebe1d901de5887809ac9719da55425082c608
riwazp21 <riwazpdel@gmail.com>
2025-03-24 18:32:42 -0500

    pushing new changes

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,12 @@
-import NextAuth from "next-auth"
+import "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
   }
 }
-
