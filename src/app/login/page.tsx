b7b33049375591408d8b2399a28a76aa8706d124
riwazp21 <riwazpdel@gmail.com>
2025-03-24 17:50:01 -0500

    Router changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@
 
 import { signIn } from "next-auth/react";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+//import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   //const router = useRouter()
