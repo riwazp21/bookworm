1eccd19fdc4a5a745ad755b811b80e48d76e156b
riwazp21 <riwazpdel@gmail.com>
2025-03-24 19:29:37 -0500

    Revert

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,11 @@
+// src/lib/prisma.ts
+
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient(
-    // @ts-ignore ✅ ignore this block during type checking
-    {
-      __internal: {
-        engine: {
-          enablePreparedStatements: false,
-        },
-      },
-    }
-  );
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
