e8ef17d5534181eade2043e61b43792479b44fdb
riwazp21 <riwazpdel@gmail.com>
2025-03-24 19:25:21 -0500

    more errors

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,7 +7,11 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    __internal: {
+      engine: {
+        enablePreparedStatements: false, // ✅ this line is key
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
